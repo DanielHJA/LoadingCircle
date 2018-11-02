b3c4dcf216a1cb4221d8fbcdc95a7f4ae187e5cc
DanielHJA <daniel.hjartstrom@gmail.com>
2018-11-02 09:27:01 +0100

    Initial Commit

---
+++ LoadingCircle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingCircle
+//
+//  Created by Daniel Hjärtström on 2018-11-02.
+//  Copyright © 2018 Sog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
